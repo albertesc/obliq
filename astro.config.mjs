aef27fe90221fe140ff44e2aa762f8249443c15a
ext-aescamilla <ext-aescamilla@cide.net>
2025-06-30 17:25:01 +0200

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+
   i18n: {
     locales: ["es"],
     defaultLocale: "es",
@@ -14,7 +17,10 @@ export default defineConfig({
       redirectToDefaultLocale: true,
     },
   },
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: vercel()
 });
