ae4e62a9a339e8fa897547e24c446e8fee429192
ext-aescamilla <ext-aescamilla@cide.net>
2025-07-29 17:11:04 +0200

    feat: mejorar manejo de errores en getAllVideos; agregar registros de consola para trazabilidad

--- src/services/videos.ts
+++ src/services/videos.ts
@@ -1,16 +1,18 @@
 const API_URL = import.meta.env.PUBLIC_API_URL;
 
 export async function getAllVideos() {
-  const data = fetch(`${API_URL}videos/all.json`);
   try {
     const response = await fetch(`${API_URL}videos/all.json`);
+    console.log(response)
     if (!response.ok) {
+      console.error(`Error fetching videos: ${response.statusText}`);
       throw new Error(`HTTP ${response.status}`);
     }
     const data = await response.json();
     console.log(data);
     return data;
   } catch (err) {
+    console.error('Error fetching videos:', err);
     return [];
   }
 }
