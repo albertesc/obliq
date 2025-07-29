acde7c99fe136c84c14d3ab518f410ababeb4759
ext-aescamilla <ext-aescamilla@cide.net>
2025-07-29 17:04:37 +0200

    feat: refactor getAllVideos para mejorar el manejo de errores y la trazabilidad de las solicitudes

--- src/services/videos.ts
+++ src/services/videos.ts
@@ -1,19 +1,18 @@
 const API_URL = import.meta.env.PUBLIC_API_URL;
 
 export async function getAllVideos() {
-  const data = fetch(`${API_URL}videos/all.json`)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`HTTP ${response.status}`);
-      }
-      return response.json();
-    })
-    .catch((error) => {
-      console.error('Error fetching videos:', error);
-      return { error: 'Failed to fetch videos' };
-    });
-
-  return data;
+  const data = fetch(`${API_URL}videos/all.json`);
+  try {
+    const response = await fetch(`${API_URL}videos/all.json`);
+    console.log(`Fetching all videos from: ${API_URL}videos/all.json`);
+    if (!response.ok) {
+      throw new Error(`HTTP ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    return [];
+  }
 }
 
 export async function getVideoBySlug(slug: string) {
